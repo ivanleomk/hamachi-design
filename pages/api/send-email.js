f1768734dd4c26f269ba1d199535afcc1565218c
Ivan Leo <ivanleomk@gmail.com>
2022-05-08 00:37:26 +0800

    Removed the pre-header text

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -160,23 +160,7 @@ const generateEmailTemplate = (message, name) => {
         -webkit-text-size-adjust: 100%;
       "
     >
-      <span
-        class="preheader"
-        style="
-          color: transparent;
-          display: none;
-          height: 0;
-          max-height: 0;
-          max-width: 0;
-          opacity: 0;
-          overflow: hidden;
-          mso-hide: all;
-          visibility: hidden;
-          width: 0;
-        "
-        >This is preheader text. Some clients will show this text as a
-        preview.</span
-      >
+      
       <table
         role="presentation"
         border="0"
